b4e186d497ba4bdfcfc62a28b6776f2e6eda8854
Jacob Menashi <jbmenashi@gmail.com>
2018-10-08 15:09:37 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -22,7 +22,14 @@ def get_contestant_name(data, occupation)
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  counter = 0 
+  data.each do |season, contestants|
+    contestants.each do |contestant|
+      if contestant["hometown"] == hometown
+        counter += 1 
+      end
+    end
+  end
 end
 
 def get_occupation(data, hometown)
