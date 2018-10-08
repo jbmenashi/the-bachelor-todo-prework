e792985681a043368748419df8bd8217bbd523ce
Jacob Menashi <jbmenashi@gmail.com>
2018-10-08 15:05:21 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -3,7 +3,7 @@ def get_first_name_of_season_winner(data, season)
     if bach_season == season
       contestants.each do |contestant|
         if contestant["status"] == "Winner"
-          return contestant["name"]
+          contestant["name"].split(" ")
         end
       end
     end
